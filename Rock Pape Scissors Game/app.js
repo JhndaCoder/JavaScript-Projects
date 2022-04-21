98fe2959da289c31217a733e4a5c7d7aead6d009
Amit Goyal <agoyal3_be21@thapar.edu>
2022-04-21 14:44:50 +0530

    Started working on Chat Bot

--- Rock Pape Scissors Game/app.js
+++ Rock Pape Scissors Game/app.js
@@ -6,3 +6,7 @@ const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const scissors_div = document.getElementById("scissors");
+
+rock_div.addEventListener('click', function(){
+    console.log("hiiiiiiii");
+})
